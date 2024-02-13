887cf5f97fd9337dbbf785d6b55ab3344afa68cd
Vimalmr <vimalmr65@gmail.com>
2024-02-13 09:57:23 +0530

    prob stat added, transition effect, poster change

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -24,3 +24,30 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modalTrigger = document.querySelectorAll('.modal-trigger1');
+    const modal = document.getElementById('modal1');
+    const modalImage = document.getElementById('modal-image1');
+    const cancelBtn = document.getElementById('cancel-btn1');
+  
+    modalTrigger.forEach(function(trigger) {
+      trigger.addEventListener('click', function(e) {
+        e.preventDefault();
+        const imageUrl = this.getAttribute('data-image');
+        modalImage.setAttribute('src', imageUrl);
+        modal.style.display = 'block';
+      });
+    });
+  
+    cancelBtn.addEventListener('click', function() {
+      modal.style.display = 'none';
+    });
+  
+    // Close modal when clicking outside of the modal content
+    window.addEventListener('click', function(e) {
+      if (e.target === modal) {
+        modal.style.display = 'none';
+      }
+    });
+  });
