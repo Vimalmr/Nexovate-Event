0eeed062851e2095f824a0d4f41efb2da32ea592
Vimalmr <vimalmr65@gmail.com>
2024-02-13 14:47:16 +0530

    full screen patch up

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -51,3 +51,30 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+      const modalTrigger = document.querySelectorAll('.modal-trigger2');
+      const modal = document.getElementById('modal2');
+      const modalImage = document.getElementById('modal-image2');
+      const cancelBtn = document.getElementById('cancel-btn2');
+    
+      modalTrigger.forEach(function(trigger) {
+        trigger.addEventListener('click', function(e) {
+          e.preventDefault();
+          const imageUrl = this.getAttribute('data-image');
+          modalImage.setAttribute('src', imageUrl);
+          modal.style.display = 'block';
+        });
+      });
+    
+      cancelBtn.addEventListener('click', function() {
+        modal.style.display = 'none';
+      });
+    
+      // Close modal when clicking outside of the modal content
+      window.addEventListener('click', function(e) {
+        if (e.target === modal) {
+          modal.style.display = 'none';
+        }
+      });
+    });
